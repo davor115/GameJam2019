63df42752b3a8ae7d55522fe883a659a28b9db14
s4911672 <36960868+s4911672@users.noreply.github.com>
2019-11-05 21:21:03 +0000

    made the fade stuff

---
+++ GameJamZombie/Assets/FadeScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FadeScript : MonoBehaviour {
+
+    public Animator animator;
+    private int LevelToLoad;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void FadeToLevel(int levelIndex)
+    {
+        LevelToLoad = levelIndex;
+        animator.SetTrigger("FadeOut");
+
+    }
+
+    public void onFadeComplete()
+
+    {
+        SceneManager.LoadScene(LevelToLoad);
+
+
+    }
+
+    public void FadeToNextLevel()
+    {
+        FadeToLevel(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+}
